fccc1030982de329bcc318a4621c18bc2937a075
VatsalMandloi <vatsal.mandloi.19@gmail.com>
2022-08-05 00:00:04 +0530

    resolving infinite progress

--- components/icon.tsx
+++ components/icon.tsx
@@ -3,24 +3,60 @@ import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import { soundExist } from './soundExist';
 import { downloadSound } from './downloadSound';
+import * as FileSystem from 'expo-file-system';
+import data from '../data/data.json';
 
 type iconProps={
     type: string,
 }
 
-
 export default function Icon({ type }: iconProps) {
 
-    const [progress, setProgress] = useState(60); 
-   
-    if (  soundExist(type)) {
-        //file exist  setUri(function uri of file)
+    const [progress, setProgress] = useState(0); 
 
-    } else {
-        //file not exist  fire caching function retrun a progress and uri
-            
+    const callback = (downloadProgress: { totalBytesWritten: number; totalBytesExpectedToWrite: number; }) => {
+        const p =downloadProgress.totalBytesWritten / downloadProgress.totalBytesExpectedToWrite;
+       
+        setProgress(p*100);
+        console.log(progress)
+        if (progress >= 1 || progress < 0) {
+         // setProgress(60);
+        }
+      };
+      
+     
+
+    const downloadSound = async() => {
+       
+            const downloadResumable = FileSystem.createDownloadResumable(
+              'https://drive.google.com/uc?export=download&id=1H-y7CRcIcedFy-Q0E5NKhxERtPa22be-',
+              FileSystem.documentDirectory + type,
+              {},
+              callback
+            );
+           
+        
+            try {
+              const  uri  = await downloadResumable.downloadAsync();
+             // setProgress(100)
+              
+              
+            } catch (e) {
+              console.error(e);
+            }
+         
     }
     
+    // if (  soundExist(type)) {
+    //     //file exist  setUri(function uri of file)
+    //     console.log("if")
+    //    //setProgress(100)
+    // } else {
+    //     //file not exist  fire caching function retrun a uri and update progress
+    //     console.log("else")
+    //     downloadSound();
+    // }
+  downloadSound();
     return (
         <View style={styles.icon}>
 
--- components/soundExist.tsx
+++ components/soundExist.tsx
@@ -1,13 +1,14 @@
 import * as FileSystem from 'expo-file-system';
-export const soundExist = (type: any) => {
+export const soundExist =  (type: any) => {
        
     const getFileInfo =  FileSystem.getInfoAsync(FileSystem.documentDirectory + type, { size: true });
-    if (!getFileInfo) {
+   //getFileInfo.exists
+    if (!( getFileInfo)) {
         console.log("false");
         return true;
        
     } else {
-        console.log(getFileInfo);
+    
         return false;
 
     }
